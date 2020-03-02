6dd2ef1b92e2db7529007ca052959b4d74b8a95d
Darren <dknibbs@retrorabbit.co.za>
2020-03-02 09:13:35 +0200

    removed unused components

--- WARM/src/app/app.module.ts
+++ WARM/src/app/app.module.ts
@@ -1,28 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AngularMaterialModule } from './angular-material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularMaterialModule } from "./angular-material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './components/login/login.component';
-import { TransactionalbankerHomeComponent } from './containers/transactionalbanker-home/transactionalbanker-home.component';
-import { TransactionalBankerModule } from './modules/transactional-banker.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LoginComponent } from "./components/login/login.component";
+import { TransactionalbankerHomeComponent } from "./containers/transactionalbanker-home/transactionalbanker-home.component";
+import { TransactionalBankerModule } from "./modules/transactional-banker.module";
 
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { environment } from "../environments/environment";
 import { MainMenuComponent } from "./components/main-menu/main-menu.component";
-import { ChatContainerComponent } from "./components/chat-container/chat-container.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     TransactionalbankerHomeComponent,
-    MainMenuComponent,
-    ChatContainerComponent
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
