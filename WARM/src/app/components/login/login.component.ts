245f7b60126f096ff6ded390cc14c06a6424a5bf
Darren <dknibbs@retrorabbit.co.za>
2020-03-03 10:43:35 +0200

    basic logic for login.component

--- WARM/src/app/components/login/login.component.ts
+++ WARM/src/app/components/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Http2ServerRequest } from 'http2';
+import {}
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  constructor(public username?: string, private password?: string) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  get pass() {
+    return this.password;
+  }
+  set pass(value) {
+    //put password constraint logic here
+    this.password = value;
   }
 
+  onClick() {
+    
+  }
 }
+
+let tryLogin = (userName, password) => {};
