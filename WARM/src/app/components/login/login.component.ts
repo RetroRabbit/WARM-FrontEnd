f00fe85e609b9d16646c9c0a73cbed1822966dac
Dzilafho Mulugisi <dziprojects@gmail.com>
2020-03-03 13:03:32 +0200

    Removed unused node modules and added form colors and validation

--- WARM/src/app/components/login/login.component.ts
+++ WARM/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.nullValidator]);
+
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.password.hasError('password') ? 'Not a valid email' : '';
+  }
 
   ngOnInit(): void {
   }
